f64fe24d110891aa3d8f0379d5f281a82d54fb75
James Gough <james@peacefulprogramming.dev>
2025-01-15 11:08:54 +0000

    ok

--- mix.exs
+++ mix.exs
@@ -44,7 +44,7 @@ defmodule GoogleApis.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:google_api_discovery, github: "peaceful-james/elixir-google-api", branch: "update-gax-deps", subdir: "clients/discovery"},
+      {:google_api_discovery, github: "peaceful-james/elixir-google-api", branch: "update-gax-deps"},
       {:tesla, "~> 1.8"},
       {:oauth2, "~> 0.9"},
       {:temp, "~> 0.4"},
